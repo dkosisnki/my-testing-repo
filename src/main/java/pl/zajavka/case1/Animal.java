432e31d195186d188f8df1b4e75f5ba226053878
Testowy Koleszka <testowykoleszka@zajavka.pl>
2023-04-08 16:33:39 +0200

    Modication 2 EDITED

--- src/main/java/pl/zajavka/case1/Animal.java
+++ src/main/java/pl/zajavka/case1/Animal.java
@@ -1,6 +1,8 @@
 package pl.zajavka.case1;
 
 public abstract class Animal {
+//    TO TU JEST BO ZMIENILEM 2 COMMITA A POZNIEJ JESZCZE JEST 3
+    private String name;
 
     protected String whatIsYourName() {
         return "I don't have any";

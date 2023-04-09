170ca0dfe22ac56c547e7349904f7169a19b6308
Testowy Koleszka <testowykoleszka@zajavka.pl>
2023-04-09 15:03:57 +0200

    My modification

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -2,10 +2,17 @@ package pl.zajavka.case2;
 
 import java.math.BigDecimal;
 
+<<<<<<< Updated upstream
 record Person(String name, Long age, BigDecimal salary) {
 
     public Boolean doYouHaveABike() {
         return true;
+=======
+record Person(String name, BigDecimal salary) {
+
+    public BigDecimal howMuchDoYouEarn() {
+        return salary;
+>>>>>>> Stashed changes
     }
 
     public Boolean doYouLikePizza() {
